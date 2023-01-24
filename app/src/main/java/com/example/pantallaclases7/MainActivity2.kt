1a3304ed960c6f020b586c1559beaf82d983aabf
javiersanchzgil <javsan675@alu.centroafuera.es>
2023-01-24 12:26:55 +0100

    CLASE2ACABADA

--- app/src/main/java/com/example/pantallaclases7/MainActivity2.kt
+++ app/src/main/java/com/example/pantallaclases7/MainActivity2.kt
@@ -1,16 +1,34 @@
 package com.example.pantallaclases7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pantallaclases7.databinding.ActivityMainBinding
+import com.example.pantallaclases7.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.orco.setOnClickListener(){
+            binding.imageView.setImageResource(R.drawable.orcoimagen)
+        }
+
+        binding.enano.setOnClickListener(){
+            binding.imageView.setImageResource(R.drawable.enanoimagen)
+        }
+
+        binding.hombre.setOnClickListener(){
+            binding.imageView.setImageResource(R.drawable.hombresimagen)
+        }
+
+        binding.aceptar2.setOnClickListener(){
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
 
 
     }
